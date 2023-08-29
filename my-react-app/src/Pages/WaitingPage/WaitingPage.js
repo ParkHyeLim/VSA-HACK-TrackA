b3e02895276db06809e4449a2cdadd57ee237e56
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-08-30 05:00:59 +0900

    [Feat]  대기 인원 표시

--- my-react-app/src/Pages/WaitingPage/WaitingPage.js
+++ my-react-app/src/Pages/WaitingPage/WaitingPage.js
@@ -2,10 +2,22 @@ import React, { useEffect, useState } from "react";
 import "./WaitingPage.css";
 
 const WaitingPage = () => {
+  const [people, setPeople] = useState(9); // 총인원
+  const [place, setPlace] = useState(9); // 현재 위치
+
+  useEffect(() => {
+    
+  }, []);
+
+  useEffect(() => {
+
+  }, [place]);
 
   return (
     <div>
       <div>대기열 페이지</div>
+      <div>총 대기 인원 : {people}</div>
+      <div>현재 나의 위치 : {place} 번째</div>
     </div>
   );
 };
