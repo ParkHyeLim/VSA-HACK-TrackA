3545b88fb00adb71a68126ebea61f56d7333dd02
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-08-30 04:08:49 +0900

    [Feat] 손가락 골격 추가

--- my-react-app/src/Pages/HandTensorflow/HandTensorflow.js
+++ my-react-app/src/Pages/HandTensorflow/HandTensorflow.js
@@ -4,16 +4,16 @@ import * as mobilenet from '@tensorflow-models/mobilenet';
 import * as handpose from '@tensorflow-models/handpose';
 import './hands.css';
 
-const ImageClassifier = () => {
-  let net;
-  let handposeModel;
+const FingerSkeleton = () => {
   const camera = useRef();
+  const canvas = useRef();
   let circleRef = useRef();
+  const netRef = useRef();
+  const handposeModelRef = useRef();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
   useEffect(() => {
-    // Update the window dimensions when the window is resized
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
       setWindowHeight(window.innerHeight);
@@ -27,13 +27,9 @@ const ImageClassifier = () => {
   }, []);
 
   useEffect(() => {
-
     const run = async () => {
-      // Load MobileNet model
-      net = await mobilenet.load();
-
-      // Load HandPose model
-      handposeModel = await handpose.load();
+      netRef.current = await mobilenet.load();
+      handposeModelRef.current = await handpose.load();
 
       // Get webcam access
       const webcam = await tf.data.webcam(camera.current, {
@@ -45,39 +41,64 @@ const ImageClassifier = () => {
         const img = await webcam.capture();
 
         // Detect hand pose
-        const handPredictions = await handposeModel.estimateHands(img);
+        const handPredictions = await handposeModelRef.current.estimateHands(img);
+
         if (handPredictions.length > 0) {
-          const handLandmarks = handPredictions[0].landmarks;
-          const [x, y] = handLandmarks[0]; // Assuming you want the position of the first landmark (hand joint)
-          if (circleRef.current) {
-            circleRef.current.style.left = x + 'px';
-            circleRef.current.style.top = y + 'px';
-          } 
+          const ctx = canvas.current.getContext("2d");
+          const landmarks = handPredictions[0].landmarks;
+
+          // Clear canvas
+          ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+
+          // Draw finger skeleton
+          for (let i = 0; i < landmarks.length; i++) {
+            const [x, y] = landmarks[i];
+
+            if (i === 8) {
+              // Set circle position using index finger coordinates
+              if (circleRef.current) {
+                circleRef.current.style.left = x + 'px';
+                circleRef.current.style.top = y + 'px';
+              }
+            }
+
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, 2 * Math.PI);
+            ctx.fillStyle = "red";
+            ctx.fill();
+
+          }
+
+          img.dispose();
+
+          await tf.nextFrame();
         }
+      };
 
-        img.dispose();
-
-        await tf.nextFrame();
-      }
-    };
-
+    }
     run();
   }, []);
 
   return (
-    <div className="container">
-      <div className="video-container">
-        <video
-          autoPlay
-          playsInline
-          muted={true}
-          ref={camera}
-          className="video"
-        />
-      </div>
+    <div>
+      <video
+        autoPlay
+        playsInline
+        muted={true}
+        ref={camera}
+        width={windowWidth}
+        height={windowHeight}
+        style={{ display: 'none' }}
+      />
+
       <div className="circle" ref={circleRef}></div>
+      <canvas
+        ref={canvas}
+        width={windowWidth}
+        height={windowHeight}
+      />
     </div>
   );
 };
 
-export default ImageClassifier;
+export default FingerSkeleton;
