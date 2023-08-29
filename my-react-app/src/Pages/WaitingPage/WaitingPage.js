cc25e1cf8cb5459155e2a57c7f12c1ae1910f7c9
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-08-30 04:40:09 +0900

    [Feat] 대기열 페이지 연습용 페이지

---
+++ my-react-app/src/Pages/WaitingPage/WaitingPage.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import "./WaitingPage.css";
+
+const WaitingPage = () => {
+
+  return (
+    <div>
+      <div>대기열 페이지</div>
+    </div>
+  );
+};
+
+export default WaitingPage;
